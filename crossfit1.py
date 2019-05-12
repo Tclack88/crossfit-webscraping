06cb8e1b1a42466513ae05775c6e95f2947a8bc6
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2019-05-12 11:00:42 -0700

    Update crossfit1.py
    more working version; Selenium issues handled
    ISSUES TO STILL FIX:
    unending timeout try again for web pages that don't exist (e.g. February 30th)

--- crossfit1.py
+++ crossfit1.py
@@ -2,48 +2,99 @@
 import time
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from multiprocessing import Process
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.managed_default_content_settings.images": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-#driver = webdriver.Chrome(chrome_options=chrome_options)
-browser = webdriver.Chrome(executable_path='/home/tclack/bin/chromedriver',chrome_options=chrome_options)
+monthDict={'01':'Jan', '02':'Feb', '03':'Mar', '04':'Apr', '05':'May', '06':'Jun', '07':'Jul', '08':'Aug', '09':'Sep', '10':'Oct', '11':'Nov', '12':'Dec'}
+
+
+browser = webdriver.Chrome('/home/tclack/bin/chromedriver')
 urlstart = "https://www.crossfit.com/workout/2009/03/"
 urlend = "#/comments"
 
-"""
-for i in range(1,31):
-    urlmiddle = str(i)
-    if len(urlmiddle) == 1:
-        urlmiddle = "0"+urlmiddle
-    url = urlstart+urlmiddle+urlend
-    p = Process(target=Collect)
-    p.start()
-    p.join()
-"""
+
+def waitForLoad(browser):
+    try:
+        WebDriverWait(browser, 10, ignored_exceptions=[
+            NoSuchElementException, TimeoutException
+        ]).until(EC.presence_of_element_located((By.CLASS_NAME,'name')))
+    except TimeoutException:
+        print("Error, timed out, trying again")
+        browser.refresh()
+        waitForLoad(browser)
+        return
+
+
+def Parse(innerHTML):
+
+    soup = BeautifulSoup(innerHTML,"html.parser")
+
+    #### Get Date #####
+    date = soup.find("h1").text
+    date = date.split(' ')
+    datestring = '20'+date[1]
+    year = datestring[0:4]
+    month = datestring[4:6]
+    month = monthDict[month]
+    day = datestring[6:]
+    print(':::::',day,month,year,':::::')
+
+    ##### Get WOD description #####
+    WOD = soup.find("div",{"class":'content'})
+    WODdescription = WOD.find_all('p')
+    for i in range(len(WODdescription)):
+        line = WODdescription[i].text
+        if line.split(' ')[0]=='Post':
+            break
+        # first few elements are title and description, often there is addition info
+        # each WOD is always like: 'Post time to comments', so I don't want that or
+        # any of the links or junk that follows"
+        else:
+            print(WODdescription[i].text)
+    print("\n")
+
+    #### My Post ####
+
+    users = soup.find_all("div",{"class":"name"})
+    posts = soup.find_all("div",{"class":"text"})
+    index = 0
+    for user in users:
+        if "clack_attack" in user.text.lower(): 
+            print(user.text)
+            # My username changes and I don't always capitalize
+            print(posts[index].text)
+        index += 1
+    print('\n'*5)
+
+
 def Collect():
     browser.get(url)
-
+    #wait = WebDriverWait(browser,10)
+    #wait.until(EC.visibility_of_element_located((By.CLASS_NAME,'name')))
+    waitForLoad(browser)
 
     innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
-
-    string = "Clack_Attack"
-    time.sleep(5)
-    if string in innerHTML:
-        print('Trevor present in',url)
+    user = "Clack_Attack"
+    if user in innerHTML:
+        #print('user present in',url)
+        Parse(innerHTML)
     else:
         print('nahbro')
+
+
 
 
 for i in range(1,31):
     urlmiddle = str(i).zfill(2)
     url = urlstart+urlmiddle+urlend
     Collect()
-    #p = Process(target=Collect, args=())
-    #p.start()
-    #p.join()
+
+
 
 
 browser.close()
